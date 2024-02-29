fed77fb4d76f96cdaad8a65aa4dfaf246b52e2f2
Marc <marc@mg.dev>
2024-02-29 15:10:12 +0100

    Ajout du fichier docker compose
    - nouveau fichier docker compose
    - ajout des fichiers d'init des 2 BDD
    - modification des dockerfiles

---
+++ init-mongodb.js
@@ -0,0 +1,11 @@
+use medilabo
+db.patient_history.drop()
+db.patient_history.insert({patientId: 1, patient: "TestNone", note: "Le patient déclare qu'il 'se sent très bien' Poids égal ou inférieur au poids recommandé"})
+db.patient_history.insert({patientId: 2, patient: "TestBorderline", note: "Le patient déclare qu'il ressent beaucoup de stress au travail Il se plaint également que son audition est anormale dernièrement"})
+db.patient_history.insert({patientId: 2, patient: "TestBorderline", note: "Le patient déclare avoir fait une réaction aux médicaments au cours des 3 derniers mois Il remarque également que son audition continue d'être anormale"})
+db.patient_history.insert({patientId: 3, patient: "TestInDanger", note: "Le patient déclare qu'il fume depuis peu"})
+db.patient_history.insert({patientId: 3, patient: "TestInDanger", note: "Le patient déclare qu'il est fumeur et qu'il a cessé de fumer l'année dernière Il se plaint également de crises d’apnée respiratoire anormales Tests de laboratoire indiquant un taux de cholestérol LDL élevé"})
+db.patient_history.insert({patientId: 4, patient: "TestEarlyOnset", note: "Le patient déclare qu'il lui est devenu difficile de monter les escaliers Il se plaint également d’être essoufflé Tests de laboratoire indiquant que les anticorps sont élevés Réaction aux médicaments"})
+db.patient_history.insert({patientId: 4, patient: "TestEarlyOnset", note: "Le patient déclare qu'il a mal au dos lorsqu'il reste assis pendant longtemps"})
+db.patient_history.insert({patientId: 4, patient: "TestEarlyOnset", note: "Le patient déclare avoir commencé à fumer depuis peu Hémoglobine A1C supérieure au niveau recommandé"})
+db.patient_history.insert({patientId: 4, patient: "TestEarlyOnset", note: "Taille, Poids, Cholestérol, Vertige et Réaction"})
